06142729756b6e8607a682611c1f8a313655b716
conashin <41155840+conashin@users.noreply.github.com>
2025-02-07 20:50:44 +0800

    hw1 v1

--- HW1/main.v
+++ HW1/main.v
@@ -1,20 +1,23 @@
 `timescale 1ns/1ps
 
-module hw1(
+module hw1(a, b, c, sel, O1);
     input a, b, c, sel;
     output O1;
-)
+    wire Net1, Net3, Net4, Net5;
+    wire D0, D1;
 
-wire Net1, Net2, Net3, Net4, Net5, Net6;
-xor D0L0(Net1, a, b);
-xor D0(Net2, Net1, c);
+    // D0 sig
+    xor D0L0(Net1, a, b);
+    //xor D0(Net2, Net1, c);
+    assign  = Net1 ^ c;
 
-or D1L0s0(Net3, a, c);
-and D1L0(Net4, Net3, b);
-and D1L1(Net5, a, c);
-and D1(Net6, Net5, Net4);
+    // D1 sig
+    or D1L0s0(Net3, a, c);
+    and D1L0(Net4, Net3, b);
+    and D1L1(Net5, a, c);
+    //and D1(Net6, Net5, Net4);
+    assign D1 = Net5 & Net4;
 
-// create mux select d0 or d1
-assign O1 = (sel) ? Net6 : Net2;
-
+    // mux select d0 or d1
+    assign O1 = (sel) ? D1 : D0;
 endmodule
